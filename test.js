b277e43b898988a1df9714f0932d5b0f4769c6cc
David <goodwid@gmail.com>
2016-06-23 20:07:53 -0700

    Getting device data

--- test.js
+++ test.js
@@ -5,14 +5,18 @@ var token;
 particle.login({username: process.env.PARTICLE_USERNAME, password:process.env.PARTICLE_PASSWORD})
   .then(data => {
     token = data.body.access_token;
-    console.log('Login completed:', token);
-    var devicesPr = particle.listDevices({auth: token});
-    return devicesPr;
+    console.log('Login completed.  Token:', token);
+    return particle.listDevices({auth: token});
   })
   .then(devices => {
-    const photon = devices[0];
-
-    console.log('Devices: ', devices);
+    const photon = devices.body[0];
+    return particle.getDevice({
+      deviceId: photon.id,
+      auth: token
+    });
+  })
+  .then(data => {
+    console.log('data: ', data);
   })
   .catch(err => {
     console.log('list devices failed:', err);
